d2ef16325cdc6a680f8558d641f51f84fdb4e136
AvagHovhannisyan2009 <avag@sportsbnb.org>
2026-01-27 23:49:18 +0000

    revert: restore code to original state before migration changes

--- supabase/migrations/20260127000002_add_game_functions.sql
+++
@@ -1,9 +0,0 @@
--- Add SQL function for incrementing game players
-CREATE OR REPLACE FUNCTION increment_game_players(game_id uuid)
-RETURNS void AS $$
-BEGIN
-  UPDATE games
-  SET current_players = current_players + 1
-  WHERE id = game_id;
-END;
-$$ LANGUAGE plpgsql SECURITY DEFINER;
